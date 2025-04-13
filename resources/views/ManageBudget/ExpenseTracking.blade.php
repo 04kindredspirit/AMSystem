09ff8214dd992ad690c1b9993a6ff9fcfa9a86e7
04kindredspirit <04.kindredspirit@gmail.com>
2025-04-13 12:16:44 +0800

    update

--- resources/views/ManageBudget/ExpenseTracking.blade.php
+++ resources/views/ManageBudget/ExpenseTracking.blade.php
@@ -26,7 +26,7 @@
                             </div>
                         </div>
 
-                        <!-- slaries -->
+                        <!-- salaries -->
                         <div class="col-12 col-sm-6 col-md-4 mb-4">
                             <div class="card shadow-sm">
                                 <div class="card-header bg-success text-white">
